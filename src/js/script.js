fc3dc8ce26af44529712d3ec92d829e6b1c77c8c
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-01-28 23:32:54 +0100

    Add new function render to script.js

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,27 @@
     booksList: Handlebars.compile(document.querySelector(select.templateOf.booksList).innerHTML),
   };
 
-  const menuContainer = document.querySelector.apply(select.containerOf.bookList);
+  const menuContainer = document.querySelector(select.containerOf.bookList);
+  const allBooks = [];
 
+  //dodaj nową funkcję render
+  const render = function(){
+    //wewnątrz render przejdź po każdym elemencie z dataSource.books
+    for(let book of dataSource.books){
+      //wewnątrz pętli zadbaj o wygenerowanie kodu HTML na podstawie szablonu oraz danych o konkretnej książce
+      const generatedHTML = templates.booksList(book);
+      console.log('generatedHTML', generatedHTML);
+      //na podstawie tego kodu HTML wygeneruj element DOM
+      const element = utils.createDOMFromHTML(generatedHTML);
+      console.log('element', element);
+      //wygenerowany element DOM dołącz jako nowe dziecko DOM do lisy .books-list
+      menuContainer.appendChild(element);
+      allBooks.push(element);
+      console.log('allBooks', allBooks);
+    }
+
+  };
+
+  render();
 
 }
