b2a820fb880d548f3c74c53bec4ee59a8273fc39
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-01-28 22:58:51 +0100

    Add new const select to script.js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+{
+  'use strict';
+
+  //przygotuj referencję do szablonu oraz listy .books-list
+  const select = {
+    templateOf: {
+      booksList: '#template-book',
+    },
+    containerOf: {
+      bookList: '.books-list',
+    },
+  };
+}
