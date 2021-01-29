4abaf8b72a3bbd86e63d4fedb24c5877b6bdfd1a
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-01-29 15:03:42 +0100

    Modify initActions funcion remove itteration

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@
   //przygotuj referencję do szablonu oraz listy .books-list
   const select = {
     templateOf: {
-      booksList: '#template-book',
+      bookTemplate: '#template-book',
     },
     containerOf: {
       bookList: '.books-list',
@@ -21,7 +21,7 @@
   };
 
   const templates = {
-    booksList: Handlebars.compile(document.querySelector(select.templateOf.booksList).innerHTML),
+    bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
 
   const menuContainer = document.querySelector(select.containerOf.bookList);
@@ -33,7 +33,7 @@
     //wewnątrz render przejdź po każdym elemencie z dataSource.books
     for(let book of dataSource.books){
       //wewnątrz pętli zadbaj o wygenerowanie kodu HTML na podstawie szablonu oraz danych o konkretnej książce
-      const generatedHTML = templates.booksList(book);
+      const generatedHTML = templates.bookTemplate(book);
       console.log('generatedHTML', generatedHTML);
       //na podstawie tego kodu HTML wygeneruj element DOM
       const element = utils.createDOMFromHTML(generatedHTML);
@@ -47,26 +47,26 @@
   };
 
   const initActions = function(){
-    for(let book of allBooks){
-      const bookCover = book.querySelector(select.book.bookImage);
-      console.log('bookCover', bookCover);
+    menuContainer.addEventListener('dblclick', function(event){
 
-      bookCover.addEventListener('dblclick', function(event){
-        event.preventDefault();
-        const id = bookCover.getAttribute(select.attribute.dataId);
-        console.log('id:', id);
+      const clickedElement = event.target.offsetParent;
+      console.log('clickedElement', clickedElement);
+      console.log(!clickedElement.classList.contains(select.book.bookImage));
 
-        if(!bookCover.classList.contains(select.class.favouriteBook)){
-          bookCover.classList.add(select.class.favouriteBook);
+      if(!clickedElement.classList.contains(select.book.bookImage)){
+        const id = clickedElement.getAttribute(select.attribute.dataId);
+        console.log('id', id);
+        if(!clickedElement.classList.contains(select.class.favouriteBook)){
+          clickedElement.classList.add(select.class.favouriteBook);
           favouriteBooks.push(id);
           console.log('favouriteBooks', favouriteBooks);
         } else {
-          bookCover.classList.remove(select.class.favouriteBook);
+          clickedElement.classList.remove(select.class.favouriteBook);
           favouriteBooks.splice(favouriteBooks.indexOf(id));
           console.log('favouriteBooks', favouriteBooks);
         }
-      });
-    }
+      }
+    });
   };
 
   render();
