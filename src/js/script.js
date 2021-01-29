bfae333e2aa534659c54712e7597d92f391021bd
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-01-30 00:17:05 +0100

    Add new function filterBooks

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 {
   'use strict';
 
-  //przygotuj referencję do szablonu oraz listy .books-list
   const select = {
     templateOf: {
       bookTemplate: '#template-book',
@@ -17,15 +16,15 @@
   };
 
   const menuContainer = document.querySelector(select.containerOf.bookList);
-  const filterWrapper = document.querySelector(select.containerOf.filters);
+  const filterWrapper = document.querySelector('.filters');
   const allBooks = [];
   const favouriteBooks = [];
   const filters = [];
 
 
-  const render = function(){
+  const render = function () {
 
-    for(let book of dataSource.books){
+    for (let book of dataSource.books) {
 
       const generatedHTML = templates.bookTemplate(book);
       console.log('generatedHTML', generatedHTML);
@@ -40,52 +39,67 @@
 
   };
 
-  const initActions = function(){
-    menuContainer.addEventListener('dblclick', function(event){
+  const initActions = function () {
+    menuContainer.addEventListener('dblclick', function (event) {
 
       const clickedElement = event.target.offsetParent;
-      console.log('clickedElement', clickedElement);
-      console.log(!clickedElement.classList.contains('.book__image'));
+      //console.log('clickedElement', clickedElement);
+      //console.log(!clickedElement.classList.contains('.book__image'));
 
-      if(!clickedElement.classList.contains('.book__image')){
+      if (!clickedElement.classList.contains('.book__image')) {
         const id = clickedElement.getAttribute('data-id');
-        console.log('id', id);
-        if(!clickedElement.classList.contains('favorite')){
+        //console.log('id', id);
+        if (!clickedElement.classList.contains('favorite')) {
           favouriteBooks.push(id);
           clickedElement.classList.add('favorite');
-          console.log('favouriteBooks', favouriteBooks);
+          //console.log('favouriteBooks', favouriteBooks);
         } else {
           favouriteBooks.splice(favouriteBooks.indexOf(id), 1);
           clickedElement.classList.remove('favorite');
-          console.log('favouriteBooks', favouriteBooks);
+          //console.log('favouriteBooks', favouriteBooks);
         }
       }
 
-      filterWrapper.addEventListener('click', function(event){
-        event.preventDefault();
-
-        const clickedElement = event.target;
-
-        if(clickedElement.tagName === 'INPUT' && clickedElement.type === 'checkbox' && clickedElement.name === 'filter'){
-          console.log(clickedElement.value);
-          console.log(filters);
-          if(clickedElement.checked){
-            filters.push(clickedElement.value);
-            filterBooks();
-          } else {
-            filters.splice(filters.indexOf(clickedElement.value), 1);
-            filterBooks();
-          }
-        }
-      });
     });
 
+    filterWrapper.addEventListener('click', function (event) {
 
+      const clickedElement = event.target;
+      console.log(clickedElement);
+
+      if (clickedElement.tagName === 'INPUT' && clickedElement.type === 'checkbox' && clickedElement.name === 'filter' ) {
+        console.log(clickedElement.value);
+        if (clickedElement.checked) {
+          filters.push(clickedElement.value);
+          filterBooks();
+          console.log(filters);
+        } else {
+          filters.splice(filters.indexOf(clickedElement.value), 1);
+          console.log(filters);
+          filterBooks();
+        }
+      }
+    });
   };
 
-  filterBooks = function(){
-
-  }
+  const filterBooks = function () {
+    for (let book of dataSource.books) {
+      const bookToBeHidden = document.querySelector('.book__image[data-id="' + book.id + '"]');
+      console.log('bookToBeHidden', bookToBeHidden);
+      let shouldBeHiden = false;
+      for (let filter of filters) {
+        if (book.details[filter]) {
+          shouldBeHiden = true;
+          break;
+        }
+      }
+      if (shouldBeHiden) {
+        bookToBeHidden.classList.add('hidden');
+      } else {
+        bookToBeHidden.classList.remove('hidden');
+      }
+    }
+  };
 
   render();
   initActions();
